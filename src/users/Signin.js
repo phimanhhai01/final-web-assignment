9d247500048a31c0f5d41deeff25398845cbef83
phimanhhai01 <19020277@vnu.edu.vn>
2021-11-23 21:33:54 +0700

    Redux-update

--- src/users/Signin.js
+++ src/users/Signin.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+
+
 
 const Signin = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const handleChangeUsername = (e) => {
+        setUsername(e.target.value);
+    }
+
+    const handleChangePassword = (e) => {
+        setPassword(e.target.value);
+    }
+
     return (
-        <h1>Sign in</h1>
+        <div>
+            <input type="text" value={username} onChange={handleChangeUsername} />
+            <input type="password" value={password} onChange={handleChangePassword}/>
+            <h1>Sign in</h1>
+        </div>
     );
 }
+
+
 
 export default Signin;
